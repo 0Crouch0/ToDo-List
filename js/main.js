ab80d9a9e196919798b546f79fec541258f45278
0Crouch0 <161517090+0Crouch0@users.noreply.github.com>
2025-06-10 21:32:06 +0300

    commit-4

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-import { allEvents } from "./eventsListeners/eventsListeners.js";
-import { addNewTask } from "./taskChangesFunctions/addNewTask.js";
-import { renderTasks } from "./taskChangesFunctions/renderTasks.js";
+import {
+  addNewTask,
+  filterTasks,
+  eventTask,
+  renderTasks,
+  allEvents,
+} from "./taskChangesFunctions/taskManager.js";
 import { lookPrev } from "./previev/lookPrev.js";
 import {
   arrTasks,
   loadFromLocalStorage,
 } from "./localStorage/saveToLocalStorage.js";
 import { updateCounters } from "./updateCounters/updateCounters.js";
-import { filterTasks } from "./taskChangesFunctions/filterTasks.js";
-import { eventTask } from "./taskChangesFunctions/eventTask.js";
 
 function init() {
   const loadedTasks = loadFromLocalStorage();
--- js/previev/lookPrev.js
+++ js/previev/lookPrev.js
@@ -1,5 +1,4 @@
 import { getElements } from "../getElements/getElements.js";
-import { arrTasks } from "../localStorage/saveToLocalStorage.js";
 
 export function lookPrev(arrTasks) {
   const hasNoTasks = arrTasks.length === 0;
